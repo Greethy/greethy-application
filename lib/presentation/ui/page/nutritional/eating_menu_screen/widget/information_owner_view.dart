0b65f8020c78d84c75247c38f405d7db7c2a0e4c
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2024-06-17 15:30:35 +0700

    :thread: update create datetime and plan name widget

--- lib/presentation/ui/page/nutritional/eating_menu_screen/widget/information_owner_view.dart
+++ lib/presentation/ui/page/nutritional/eating_menu_screen/widget/information_owner_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:greethy_application/presentation/state/eating_menu_screen_state.dart';
-import 'package:greethy_application/presentation/state/nutrition_home_screen_state.dart';
-
 import 'package:greethy_application/presentation/theme/theme.dart';
 import 'package:greethy_application/presentation/ui/page/profile/local_widgets/circular_image.dart';
 import 'package:greethy_application/presentation/ui/page/profile/profile_image_view.dart';
@@ -22,157 +20,206 @@ class InformationOwner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-        animation: animationController!,
-        builder: (BuildContext context, Widget? child) {
-          return FadeTransition(
-            opacity: animation!,
-            child: Transform(
+      animation: animationController!,
+      builder: (BuildContext context, Widget? child) {
+        return FadeTransition(
+          opacity: animation!,
+          child: Transform(
               transform: Matrix4.translationValues(0.0, 30 * (1.0 - animation!.value), 0.0),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      state.eatingPlan?.eatingPlanName ?? "",
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
+              child: Column(
+                children: [
+                  Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
                         color: GreethyColor.kawa_green,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: Offset(0, 20),
+                          ),
+                        ],
                       ),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      state.eatingPlan!.bio!,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w300,
-                        fontSize: 16.0,
-                        letterSpacing: 0.5,
-                        color: AppTheme.lightText,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        // Avatar
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: GreethyColor.kawa_green,
-                                width: 2.0,
-                              ), // Inner green border
-                              shape: BoxShape.circle,
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            "THỰC ĐƠN",
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
                             ),
-                            child: Container(
-                              alignment: Alignment.topLeft,
-                              child: AnimatedContainer(
-                                duration: const Duration(milliseconds: 500),
-                                padding: const EdgeInsets.all(5.0),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            state.eatingPlan?.eatingPlanName ?? "",
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          state.eatingPlan!.bio!,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 18.0,
+                            letterSpacing: 0.5,
+                            color: GreethyColor.kawa_green,
+                          ),
+                        ),
+                        SizedBox(height: 25),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            // Avatar
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Container(
                                 decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.white, width: 5.0),
+                                  border: Border.all(
+                                    color: GreethyColor.kawa_green,
+                                    width: 2.0,
+                                  ), // Inner green border
                                   shape: BoxShape.circle,
                                 ),
-                                child: RippleButton(
-                                  child: CircularImage(
-                                    path: state.eatingPlan!.owner!.avatar!,
-                                    height: 45,
+                                child: Container(
+                                  alignment: Alignment.topLeft,
+                                  child: AnimatedContainer(
+                                    duration: const Duration(milliseconds: 500),
+                                    padding: const EdgeInsets.all(5.0),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.white, width: 1.0),
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: RippleButton(
+                                      child: CircularImage(
+                                        path: state.eatingPlan!.owner!.avatar!,
+                                        height: 50,
+                                      ),
+                                      borderRadius: BorderRadius.circular(50.0),
+                                      onPressed: () {
+                                        Navigator.push(
+                                          context,
+                                          ProfileImageView.getRoute(state.eatingPlan!.owner!.id!),
+                                        );
+                                      },
+                                    ),
                                   ),
-                                  borderRadius: BorderRadius.circular(50.0),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16.0),
+                            // owner name
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    state.eatingPlan!.owner!.name!,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w900,
+                                      fontSize: 18.0,
+                                      letterSpacing: 0.5,
+                                      color: GreethyColor.kawa_green,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Khởi tạo: " + state.eatingPlan!.createDate!,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w300,
+                                      fontSize: 16.0,
+                                      letterSpacing: 0.5,
+                                      color: GreethyColor.kawa_green,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16.0),
+                        // Setting button and Customize button
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            // Customize button
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  border: Border.all(
+                                    color: GreethyColor.kawa_green,
+                                    width: 2.0,
+                                  ),
+                                ),
+                                child: TextButton(
                                   onPressed: () {
-                                    Navigator.push(
-                                      context,
-                                      ProfileImageView.getRoute(state.eatingPlan!.owner!.id!),
-                                    );
+                                    print("Nhấn nút Tùy chỉnh");
                                   },
+                                  child: Text(
+                                    'Tùy chỉnh',
+                                    style: TextStyle(
+                                      color: GreethyColor.kawa_green,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ),
-                        SizedBox(width: 16.0),
-                        // Tên người sở hữu
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                state.eatingPlan!.owner!.name!,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w900,
-                                  fontSize: 18.0,
-                                  letterSpacing: 0.5,
-                                  color: GreethyColor.kawa_green,
+                            SizedBox(width: 16.0),
+                            // Setting button
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  border: Border.all(
+                                    color: GreethyColor.kawa_green,
+                                    width: 2.0,
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 16.0),
-                    // Dòng chứa nút "Tùy chỉnh" và "Cài đặt"
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        // Nút Tùy chỉnh
-                        Expanded(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20.0),
-                              border: Border.all(color: Colors.blue, width: 2.0),
-                            ),
-                            child: TextButton(
-                              onPressed: () {
-                                // Xử lý khi nhấn nút Tùy chỉnh
-                                print("Nhấn nút Tùy chỉnh");
-                              },
-                              child: Text(
-                                'Tùy chỉnh',
-                                style: TextStyle(
-                                  color: Colors.blue,
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.bold,
+                                child: TextButton(
+                                  onPressed: () {
+                                    print("Nhấn nút Cài đặt");
+                                  },
+                                  child: Text(
+                                    'Cài đặt',
+                                    style: TextStyle(
+                                      color: GreethyColor.kawa_green,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ),
-                        SizedBox(width: 16.0), // Khoảng cách giữa các nút
-                        // Nút Cài đặt
-                        Expanded(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20.0),
-                              border: Border.all(color: Colors.blue, width: 2.0),
-                            ),
-                            child: TextButton(
-                              onPressed: () {
-                                // Xử lý khi nhấn nút Cài đặt
-                                print("Nhấn nút Cài đặt");
-                              },
-                              child: Text(
-                                'Cài đặt',
-                                style: TextStyle(
-                                  color: Colors.blue,
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
-                ),
-              ),
-            ),
-          );
-        },
-      );
+                  ),
+                ],
+              )),
+        );
+      },
+    );
   }
 }

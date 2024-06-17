2d1de487e55e51d13472ad6e8e48df1577f44373
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2024-06-17 15:31:01 +0700

    :art: add new widget in menu diary

--- lib/presentation/ui/page/nutritional/eating_menu_screen/menu_diary_screen.dart
+++ lib/presentation/ui/page/nutritional/eating_menu_screen/menu_diary_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:greethy_application/domain/entities/nutrition_entities/food_menu.dart';
+import 'package:greethy_application/presentation/helper/ui_helper.dart';
 import 'package:greethy_application/presentation/state/eating_menu_screen_state.dart';
+import 'package:greethy_application/presentation/theme/app_colors.dart';
 import 'package:greethy_application/presentation/theme/theme.dart';
 import 'package:greethy_application/presentation/ui/page/nutritional/eating_menu_screen/widget/custom_divider_meal_view.dart';
 import 'package:greethy_application/presentation/ui/page/nutritional/eating_menu_screen/widget/food_view.dart';
@@ -8,7 +10,9 @@ import 'package:greethy_application/presentation/ui/page/nutritional/eating_menu
 import 'package:greethy_application/presentation/ui/page/nutritional/eating_menu_screen/widget/quote_view.dart';
 import 'package:greethy_application/presentation/ui/page/nutritional/eating_menu_screen/widget/total_nutrition_per_day_view.dart';
 import 'package:greethy_application/presentation/ui/page/nutritional/widget/footnote_view.dart';
+import 'package:greethy_application/test_library/test%20restaurant%20ui/nutrition_home_detail/menu/all_restaurants/all_restaurants_screen.dart';
 import 'package:greethy_application/test_library/test%20restaurant%20ui/nutrition_home_detail/menu/menu_screen.dart';
+import 'package:greethy_application/test_library/test%20restaurant%20ui/nutrition_home_detail/menu/offers/offer_banner_view.dart';
 import 'package:provider/provider.dart';
 
 class MenuDiaryScreen extends StatefulWidget {
@@ -112,6 +116,143 @@ class _MenuDiaryScreenState extends State<MenuDiaryScreen> with TickerProviderSt
     listViews = [];
 
     listViews.add(
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+        child: Row(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(8.0),
+                bottomRight: Radius.circular(8.0),
+              ),
+              child: Container(
+                width: 10.0,
+                height: 140.0,
+                color: menuGreen,
+              ),
+            ),
+            UIHelper.horizontalSpaceMedium(),
+            Flexible(
+              child: Column(
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                      style: Theme.of(context).textTheme.headlineMedium,
+                      children: [
+                        TextSpan(text: 'Hành trình chăm sóc sức khỏe dinh dưỡng cùng '),
+                        TextSpan(text: 'Greethy.', style: TextStyle(color: GreethyColor.kawa_green)),
+                      ],
+                    ),
+                  ),
+                  UIHelper.verticalSpaceSmall(),
+                  Text(
+                    'Chăm sóc sức khỏe dinh dưỡng của bản thân là một yếu tố quan trọng để duy trì sức khỏe tốt và cải thiện chất lượng cuộc sống.',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          fontSize: 16.0,
+                          color: Colors.grey[800],
+                        ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+
+    listViews.add(
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+        child: Stack(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AllRestaurantsScreen(),
+                    ),
+                  );
+                },
+                child: Container(
+                  height: 170.0,
+                  color: menuGreen,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: FractionallySizedBox(
+                          widthFactor: 0.7,
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  'Menu',
+                                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: Colors.white),
+                                ),
+                                UIHelper.verticalSpaceSmall(),
+                                Text(
+                                  'Sổ tay ẩm thực dinh dưỡng, đồng hành cùng bạn.',
+                                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(color: Colors.white),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      const Spacer(),
+                      Container(
+                        height: 45.0,
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        color: darkGreen,
+                        child: Row(
+                          children: <Widget>[
+                            Text(
+                              'Tra cứu',
+                              style: Theme.of(context).textTheme.bodyLarge!.copyWith(color: Colors.white, fontSize: 18.0),
+                            ),
+                            UIHelper.horizontalSpaceSmall(),
+                            const Icon(
+                              Icons.arrow_forward,
+                              color: Colors.white,
+                              size: 18.0,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: -10.0,
+              right: -10.0,
+              child: ClipOval(
+                child: Image.asset(
+                  'assets/images/food1.jpg',
+                  width: 130.0,
+                  height: 130.0,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    listViews.add(
+      OfferBannerView(),
+    );
+
+    listViews.add(
       InformationOwner(
         state: state,
         animation: Tween<double>(begin: 0.0, end: 1.0).animate(
@@ -141,7 +282,7 @@ class _MenuDiaryScreenState extends State<MenuDiaryScreen> with TickerProviderSt
     if (state.breakfast != null) {
       listViews.add(
         CustomDividerMealView(
-          dividerHeight: 35,
+          dividerHeight: 40,
           text: "BỮA SÁNG",
           meal: state.breakfast,
         ),
@@ -166,7 +307,7 @@ class _MenuDiaryScreenState extends State<MenuDiaryScreen> with TickerProviderSt
     if (state.morningSnack != null) {
       listViews.add(
         CustomDividerMealView(
-          dividerHeight: 35,
+          dividerHeight: 40,
           text: "BỮA PHỤ SÁNG",
           meal: state.morningSnack,
         ),
@@ -191,7 +332,7 @@ class _MenuDiaryScreenState extends State<MenuDiaryScreen> with TickerProviderSt
     if (state.lunch != null) {
       listViews.add(
         CustomDividerMealView(
-          dividerHeight: 35,
+          dividerHeight: 40,
           text: "BỮA TRƯA",
           meal: state.lunch,
         ),
@@ -216,7 +357,7 @@ class _MenuDiaryScreenState extends State<MenuDiaryScreen> with TickerProviderSt
     if (state.afternoonSnack != null) {
       listViews.add(
         CustomDividerMealView(
-          dividerHeight: 35,
+          dividerHeight: 40,
           text: "BỮA PHỤ CHIỀU",
           meal: state.afternoonSnack,
         ),
@@ -241,7 +382,7 @@ class _MenuDiaryScreenState extends State<MenuDiaryScreen> with TickerProviderSt
     if (state.dinner != null) {
       listViews.add(
         CustomDividerMealView(
-          dividerHeight: 35,
+          dividerHeight: 40,
           text: "BỮA TỐI",
           meal: state.dinner,
         ),
@@ -323,6 +464,7 @@ class _MenuDiaryScreenState extends State<MenuDiaryScreen> with TickerProviderSt
                     color: AppTheme.white.withOpacity(topBarOpacity),
                     borderRadius: const BorderRadius.only(
                       bottomLeft: Radius.circular(32.0),
+                      bottomRight: Radius.circular(32.0),
                     ),
                     boxShadow: <BoxShadow>[
                       BoxShadow(
